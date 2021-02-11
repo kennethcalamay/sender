94831925df7f23af410482e5ade7fe05436a354a
Bjorn <git@kennethcalamay.com>
2021-02-11 17:55:42 +0800

    Finish chapter 1

--- lib/sender.ex
+++ lib/sender.ex
@@ -15,4 +15,20 @@ defmodule Sender do
   def hello do
     :world
   end
+
+  def send_email("konnichiwa@world.com" = email) do
+    raise "Oops, couldn't send email to #{email}!"
+  end
+
+  def send_email(email) do
+    Process.sleep(3_000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_sent"}
+  end
+
+  def notify_all(emails) do
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1)
+    |> Enum.to_list()
+  end
 end
